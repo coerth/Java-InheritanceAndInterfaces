a0dafccff216a7b9acc2d810f80823fd88706e15
Morten <59032722+coerth@users.noreply.github.com>
2021-10-12 11:14:05 +0200

    fork

--- src/GunInterfaceExample/Main.java
+++ src/GunInterfaceExample/Main.java
@@ -7,5 +7,9 @@ public class Main
     public static void main(String[] args)
     {
         WeaponHandler weaponHandler = new WeaponHandler();
+
+        Weapon equippedWeapon = new Handgun();
+
     }
+
 }
